e42de3316cfce6ca5aaec36be4efa75241846e6f
Nikki Bright <nikkistorme@gmail.com>
2022-11-14 16:27:40 -0600

    Add ability to accept structures of subdirectories

--- index.js
+++ index.js
@@ -1,35 +1,86 @@
 import sharp from "sharp";
-import fs from "fs";
+import { existsSync } from "fs";
+import { appendFile, readdir, mkdir } from "fs/promises";
+import { basename, extname } from "path";
 import params from "./params.js";
 
-const getFileExtension = (fileName) => {
-  return fileName.split(".").pop();
+const convert = async (inputImg, outputDir) => {
+  const newFileName = `${outputDir}/${basename(inputImg, extname(inputImg))}`;
+  try {
+    await sharp(inputImg)
+      .webp({ quality: params.compress.quality })
+      .toFile(`${newFileName}.webp`);
+    console.log("🎉 ~ success for", newFileName);
+  } catch (error) {
+    console.log("🚫 ~ error for", inputImg);
+    throw error;
+  }
 };
 
-const getFileNameWithoutExtension = (fileName) => {
-  return fileName.split(".").shift();
+const convertAndResize = async (inputImg, outputDir, size) => {
+  const newFileName = `${outputDir}/${basename(inputImg, extname(inputImg))}`;
+  try {
+    if (!existsSync(`${newFileName}.webp`))
+      await sharp(inputImg)
+        .webp({ quality: params.compress.quality })
+        .toFile(`${newFileName}.webp`);
+    await sharp(inputImg)
+      .webp({ quality: params.compress.quality })
+      .resize({ width: size })
+      .toFile(`${newFileName}-${size}.webp`);
+    console.log("🎉 ~ success for", newFileName);
+  } catch (error) {
+    console.log("🚫 ~ error for", inputImg);
+    throw error;
+  }
 };
 
-const convertAndResize = async (img, size) => {
-  const fileName = getFileNameWithoutExtension(img.name);
-  await sharp(`${params.input.dir}/${img.name}`)
-    .webp({ quality: 75 })
-    .resize({ width: size })
-    .toFile(`./output-images/${fileName}-${size}.webp`);
-};
-
-const main = async () => {
-  const inputImages = fs
-    .readdirSync(params.input.dir, { withFileTypes: true })
-    .filter((dirent) => dirent.isFile())
-    .map((dirent) => {
-      return { name: dirent.name, ext: getFileExtension(dirent.name) };
-    });
-  inputImages.forEach(async (img) => {
-    params.output.sizes.forEach(
-      async (size) => await convertAndResize(img, size)
+const optimizeImage = async (inputImg, outputDir) => {
+  try {
+    if (!params.resize.enabled) await convert(inputImg, outputDir);
+    else
+      params.resize.sizes.forEach(
+        async (size) => await convertAndResize(inputImg, outputDir, size)
+      );
+  } catch (error) {
+    await appendFile(
+      `${params.output.dir}/errors.txt`,
+      `${inputImg}\n`,
+      "utf8"
     );
+  }
+};
+
+const direntIsImage = (dirent) => {
+  return (
+    dirent.isFile() && [".jpg", ".jpeg", ".png"].includes(extname(dirent.name))
+  );
+};
+
+const parseDir = async (inputDir, outputDir) => {
+  // Read input directory
+  const inputDirEntries = await readdir(inputDir, { withFileTypes: true });
+  inputDirEntries.forEach(async (dirent) => {
+    // path to this file or directory
+    const newInput = `${inputDir}/${dirent.name}`;
+    if (dirent.isDirectory()) {
+      // if this is a directory, create a new directory in the output directory
+      // and call parseDir recursively
+      const newOutput = `${outputDir}/${dirent.name}`;
+      if (!existsSync(newOutput)) await mkdir(newOutput);
+      await parseDir(newInput, newOutput);
+    } else if (direntIsImage(dirent)) {
+      // if this is an image file, optimize it
+      await optimizeImage(newInput, outputDir);
+    }
   });
 };
 
-main();
+const main = async () => {
+  if (!existsSync(params.input.dir)) await mkdir(params.input.dir);
+  if (!existsSync(params.output.dir)) await mkdir(params.output.dir);
+
+  await parseDir(params.input.dir, params.output.dir);
+};
+
+await main();
